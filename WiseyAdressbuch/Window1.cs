cb8052fa4f934fef17afda3e1e4fd7edef57451d
YilVonMotz <yilbi@gmx.de>
2021-08-15 20:39:09 +0200

    Projektdateien hinzufügen.

---
+++ WiseyAdressbuch/Window1.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using Wisej.Web;
+
+namespace WiseyAdressbuch
+{
+    public partial class Window1 : Form
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+}
